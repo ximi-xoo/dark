c1604a061a89286b2f4c8a2441c4b878bc099a76
Bangladesh Cyber Mafia ( B.C.M) <55280273+ximi-xoo@users.noreply.github.com>
2019-11-07 23:35:30 +0600

    dark new tool
    New tool for fb hack bye Virus 07

---
+++ password.py
@@ -0,0 +1,70 @@
+import os, sys
+
+print ("\033[1;32mSilahkan To Get Username & Password ")
+
+print ("\033[1;32Follow Us on youtube and fb ")
+
+
+print ("\033[1;32mScript coded  by Ariyan ")
+
+username = 'ariyan'      
+
+password = 'nila'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mAlhmdllh sudah masuk juga..", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[1;32mMaaf Masukkan password Anda salah... [?]\033[00m"
+
+			print "Silahkan segera log-in kembali...!!\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mMaaf Masukkan Username Anda salah... [?]\033[00m"
+
+		print "Silahkan segera log-in kembali...!!\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
