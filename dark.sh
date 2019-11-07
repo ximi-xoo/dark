33a4308bb695ac84764a34fdcbe4a247232c9d04
Bangladesh Cyber Mafia ( B.C.M) <55280273+ximi-xoo@users.noreply.github.com>
2019-11-07 23:41:01 +0600

    dark.sh
    Hack Fb

---
+++ dark.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+clear
+echo
+python2 password.py
+clear
+echo
+echo -e "\e[4;31m Virus 07 !!! \e[0m"
+echo -e "\e[1;34m Presents \e[0m"
+echo -e "\e[1;32m fb king \e[0m"
+echo "CODED BY NILA & ARIYAN"
+echo
+cyan='\033[36;1m'
+ungu='\033[35;1m'
+figlet VIRUS | lolcat
+figlet 07 | lolcat
+sleep 2
+clear
+echo $ungu "
+===================================="
+echo $cyan "
+          _/﹋\_
+          (ಠ益ಠ)
+          <,︻╦╤─ - - - HACK FB BY VIRUS 07
+          _/﹋\_"
+echo $ungu "
+===================================="
+sleep 2
+
+echo $cyan "
+┏━━━━━┫{menu dark fb}┣━━━━━┓
+┃
+┠──[0] instal all pkg ✔
+┃
+┠──[1] Virus ✔
+┃
+┠──[2] Jimi  ✔
+┃
+┠──[3] Dark fb [Ariyan] ✔
+┃
+┠──[4] Dark Diamond  ✔
+┠
+┗────[99] exit ✘"
+echo "\033[35;1m"
+read -p "root@Pilih Nomor > " bro
+if [ $bro = 0 ] || [ $bro]
+then
+clear
+apt update && upgrade -y
+apt install git -y
+pkg install python2
+git clone https://github.com/ximi-xoo/Jimi
+pkg install ruby
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+pip2 install requirements
+git clone https://github.com/wira2611/diamon
+git clone https://github.com/Mr-XsZ/Dark-Fb
+sh yasin.sh
+fi
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+python2 virus.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+cd Jimi
+python2 JIMI.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+cd Dark-Fb
+python2 dark.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+cd diamon
+ls
+python2 diamond-1.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+pkg install git
+git clone https://github.com/m4rche3ll-cyber/dark-vpro
+cd dark-vpro
+python2 dark-vpro.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+git clone https://github.com/wira2611/Pro2611
+cd Pro2611
+python2 Pro.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+git clone https://github.com/wira2611/diamon
+ls
+cd diamon
+ls
+python2 diamond-1.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+git clone https://github.com/cyber2611/v1.2
+ls
+cd v1.2
+ls
+python2 Dragon-v1.2.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+git clone https://github.com/cyber2611/v2-dark
+cd v2.dark
+ls
+python2 v2.dark.py
+if
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-cyber11
+cd dark-cyber11
+ls
+python2 kapten.py
+if
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+git clone https://github.com/cyber2611/cyber2.5
+cd cyber2.5
+ls
+python2 Premium2.5.py
+if
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+git clone https://github.com/cyber2611/IBLI2
+cd IBLI2
+ls
+python2 dark-iblis.py
+if
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-asu
+cd dark-asu
+ls
+python2 kimak.py
+if
+
+if if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+git clone https://github.com/cyber2611/BHV26
+cd BHV26
+ls
+python2 vip-1.9.py
+if
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+git clone https://github.com/cyber2611/Mr.RENDY
+cd Mr.RENDY
+ls
+python2 Mr.rendy.py
+if
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "We Are Family"
+sleep 1
+exit
+fi
